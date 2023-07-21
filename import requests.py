3ebe3cdecc39777533498d5fef7ab0b3f546c7df
Diablo-the-Darklord <140195184+Diablo-the-Darklord@users.noreply.github.com>
2023-07-22 01:25:44 +0530

    Weather forecast.py

--- import requests.py
+++ import requests.py
@@ -1,37 +1,60 @@
-import cv2
-import albumentations as A
-from matplotlib import pyplot as plt
+import requests
+from datetime import datetime, timedelta
 
-# Sample image path
-image_path = "C:\Users\aksha\OneDrive\Pictures\Wallpaper\4k-blue-goku-2021-dragon-ball-anime-wallpaper-11628961535wjbiwuvknh.jpg"
+def get_weather(api_key, city, start_time, end_time):
+    base_url = "http://api.openweathermap.org/data/2.5/forecast"
+    params = {"q": city, "appid": api_key, "units": "metric"}
+    
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()  # Raise an exception for HTTP errors (if any)
+        data = response.json()
+        
+        # Extract relevant information from the API response
+        forecast_list = data["list"]
+        weather_forecast = {}
+        for forecast in forecast_list:
+            forecast_time = datetime.fromtimestamp(forecast["dt"])
+            if start_time <= forecast_time < end_time:
+                # Group forecast data by 8-hour intervals
+                interval = (forecast_time - start_time) // timedelta(hours=8)
+                if interval not in weather_forecast:
+                    weather_forecast[interval] = []
+                weather_forecast[interval].append({
+                    "description": forecast["weather"][0]["description"],
+                    "temperature": forecast["main"]["temp"],
+                    "humidity": forecast["main"]["humidity"],
+                    "wind_speed": forecast["wind"]["speed"]
+                })
+        
+        return weather_forecast
+    except requests.exceptions.RequestException as e:
+        print("Error connecting to the weather API:", e)
+        return None
+    except KeyError as e:
+        print("Error parsing API response:", e)
+        return None
 
-# Load the image
-image = cv2.imread(image_path)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB (Albumentations expects RGB)
-
-# Define a list of augmentations
-transform = A.Compose([
-    A.RandomCrop(width=256, height=256),
-    A.HorizontalFlip(p=0.5),
-    A.Rotate(limit=30, p=0.5),
-    A.RandomBrightnessContrast(p=0.2),
-    A.RandomGamma(p=0.2),
-])
-
-# Apply the augmentations
-augmented = transform(image=image)
-augmented_image = augmented["image"]
-
-# Display the original and augmented images
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-plt.imshow(image)
-plt.title("Original Image")
-plt.axis("off")
-
-plt.subplot(1, 2, 2)
-plt.imshow(augmented_image)
-plt.title("Augmented Image")
-plt.axis("off")
-
-plt.show()
+if __name__ == "__main__":
+    api_key = "ef1f2a01e7ded25ecbd177b8be02e3c3"  
+    city_name = input("Enter the city name for weather forecast: ")
+    
+    current_time = datetime.now()
+    start_time = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
+    end_time = start_time + timedelta(days=4)
+    
+    weather_data = get_weather(api_key, city_name, start_time, end_time)
+    
+    if weather_data:
+        print("\nWeather Forecast for", city_name, "over the next 4 days (8-hour intervals):")
+        for interval, forecasts in weather_data.items():
+            interval_time = start_time + timedelta(hours=interval * 8)
+            print("\nInterval starting at", interval_time.strftime("%Y-%m-%d %H:%M:%S"))
+            for forecast in forecasts:
+                print("Description:", forecast["description"])
+                print("Temperature (°C):", forecast["temperature"])
+                print("Humidity (%):", forecast["humidity"])
+                print("Wind Speed (m/s):", forecast["wind_speed"])
+                print("-" * 30)
+    else:
+        print("Unable to fetch weather forecast. Please check the city name and your API key.")
